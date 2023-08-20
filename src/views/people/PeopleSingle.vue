8d37202c38473d882eac6c3ac8d4d1da87169900
Олег Капранов <arg0nny@mail.ru>
2023-08-20 18:21:34 +0700

    People single page

--- src/views/people/PeopleSingle.vue
+++ src/views/people/PeopleSingle.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>people-single</div>
+  <AsyncWrapper :status="status" :error="error">
+    <PeopleTable v-if="people" :people="[people]" :favorite="favorite" />
+  </AsyncWrapper>
 </template>
 
 <script setup lang="ts">
+import useAsyncData from '@/composables/useAsyncData'
+import { getPeopleById } from '@/api/people'
+import { useRoute } from 'vue-router'
+import AsyncWrapper from '@/components/AsyncWrapper.vue'
+import PeopleTable from '@/components/PeopleTable.vue'
+import useFavorite from '@/composables/useFavorite'
 
+const route = useRoute()
+
+const { data: people, status, error } = useAsyncData(() => getPeopleById(route.params.id as string))
+const favorite = useFavorite()
 </script>
